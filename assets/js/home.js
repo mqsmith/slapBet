02b6569a15b67252c875cc0c1cc742175866ca3e
Tony C <tonychiesa92@gmail.com>
2019-07-17 12:11:12 -0400

    Dropdown button is working and MLS button generates an api call

--- assets/js/home.js
+++ assets/js/home.js
@@ -66,7 +66,7 @@ function writeUserData(email, password) {
   }
 
 // *** D3 API CALL ***
-function importData()
+function importSoccerData()
 {
   var queryURL = "https://api.the-odds-api.com/v3/odds?sport=soccer_usa_mls&region=us&apiKey=afd1f6803bcc123bffedb1e448fed02d";
 
@@ -86,5 +86,34 @@ function importData()
     };
   })
 }
+// function importFootballData()
+// {
+//   var queryURL = "https://api.the-odds-api.com/v3/odds?sport=soccer_usa_mls&region=us&apiKey=afd1f6803bcc123bffedb1e448fed02d";
 
-// importData();
+//   d3.json(queryURL, function (data)
+//   {
+//     var gameBetween=data.data[0].teams;
+//     var h2hArray=data.data[0].sites[0].odds.h2h;
+    
+//     console.log(data.data);
+
+//     for(i=0;i<data.data.length; i++){
+//         $(".matches").append("<tr class='well'><th class='teams'> " + data.data[i].teams +
+//         " </td><td class='odds-data'> " + data.data[i].sites[0].odds.h2h);
+//       console.log(data.data[i].teams);
+//       console.log(data.data[i].sites[0].odds.h2h);
+      
+//     };
+//   })
+// }
+
+ $(".dropdown-menu").on("click", "#mls-button", function (){
+ 
+ importSoccerData();
+ });
+
+//  $(".dropdown-menu").on("click", "#nfl-button", function (){
+ 
+//   importFootballData();
+//   });
+ 
